7828e6e6575f83bc29148c5630a8d3118a753f0a
Efren <efren.cx@gmail.com>
2023-09-14 09:35:11 -0600

    1er commit de prueba

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0)
 
-  return (
+	return (
 		<div>
-			<h1>Repaso Módulo 2</h1>
+			<h1>Repaso Módulo 2 EPISODIOS</h1>
 			<hr />
 			<h2>Cohorte FT42b</h2>
 			<h2>Rick & Morty - Episodes</h2>
 			<h3>Ver README.md para ver detalles del proyecto</h3>
 		</div>
-  )
+	)
 }
 
 export default App
